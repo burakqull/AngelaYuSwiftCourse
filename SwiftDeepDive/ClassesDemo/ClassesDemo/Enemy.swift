26d2f506c5d6bba28fd00d8ee4503ed6d6948d33
burakqull <83167665+burakqull@users.noreply.github.com>
2023-02-07 02:10:17 +0300

    Add files via upload

---
+++ SwiftDeepDive/ClassesDemo/ClassesDemo/Enemy.swift
@@ -0,0 +1,16 @@
+class Enemy {
+    
+    var health=100
+    var attackStrenght=10
+    
+    
+    func move()   {
+        print("Walk Forwards.")
+    }
+    
+    
+    func attack()  {
+        print("Land a hit ,does \(attackStrenght) damage.")
+    }
+    
+}
---
+++ SwiftDeepDive/ClassesDemo/ClassesDemo/main.swift
@@ -0,0 +1,10 @@
+let skeleton = Enemy()
+
+
+let dragon=Dragon()
+dragon.wingSpan=5
+dragon.attackStrenght=15
+dragon.talk(speech: "I came broo")
+dragon.attack()
+dragon.move()
+
